a5c6dd000cf9e1882b21e58778c22f6e6eef96dd
Marianela Holsbach <92831767+Megari94@users.noreply.github.com>
2024-06-17 22:16:27 -0300

    grilla e imagen de fondo

--- Resto.NET-TPI/Form1.cs
+++ Resto.NET-TPI/Form1.cs
@@ -2,11 +2,14 @@ namespace Resto.NET_TPI
 {
     public partial class Form1 : Form
     {
-        
+
 
         public Form1()
         {
             InitializeComponent();
+            miGrilla.Visible = false;//Por defecto se va a iniciar apagada 
+            miGrilla.BackgroundImageLayout = ImageLayout.Stretch;// Ajustar imagen al tamaño
+            panelDiseño.BackgroundImageLayout= ImageLayout.Stretch;
             
         }
 
@@ -17,7 +20,21 @@ namespace Resto.NET_TPI
 
         private void cargarPlanoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-             
+
+        }     
+
+
+
+        private void modoToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            miGrilla.Visible = false;
+            
+        }
+
+        private void modoEdiciónToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            miGrilla.Visible = true;
+            
         }
     }
 }
